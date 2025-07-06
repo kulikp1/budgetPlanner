5b47f366eeb1d5e4e95390045799b93f865e06d0
kulikp1 <pashakylik2@gmail.com>
2025-07-06 23:52:49 +0300

    Update SignUp.jsx

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -137,7 +137,6 @@ const SignUp = ({ onSwitch }) => {
             )}
           </Field>
 
-          {/* Password */}
           <Field name="password">
             {({ field, meta, form }) => (
               <>
