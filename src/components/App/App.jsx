76ff25cfa77165303314d54baa00b51cda721e95
kulikp1 <pashakylik2@gmail.com>
2025-07-07 23:16:46 +0300

    add ToastContainer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
 import TrackerPage from "../../pages/TrackerPage/TrackerPage"
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/trackerPage" element={<TrackerPage />} />
       </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
     </Router>
   );
 }
